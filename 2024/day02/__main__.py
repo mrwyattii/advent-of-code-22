b369cdb78e9f2d3de7cb621b41113b0b76c6b710
Michael Wyatt <michael.wyatt@snowflake.com>
2024-12-03 14:18:35 -0800

    AOC 24-2

---
+++ 2024/day02/__main__.py
@@ -0,0 +1,38 @@
+from typing import List
+
+from common.aoc_day import AoCDay
+
+
+class Day02(AoCDay):
+    def process_input(self, raw_input: List[str]) -> List[List[int]]:
+        return list(map(lambda x: list(map(int, x.split())), raw_input))
+
+    @staticmethod
+    def is_safe(report: List[int]) -> bool:
+        def get_sign(val: int) -> str:
+            return "+" if val >= 0 else "-"
+
+        sign = get_sign(report[1] - report[0])
+
+        for i in range(0, len(report) - 1):
+            diff = report[i + 1] - report[i]
+            if not ((get_sign(diff) == sign) and (1 <= abs(diff) <= 3)):
+                return False
+
+        return True
+
+    def part1(self, input: List[List[int]]) -> None:
+        print(sum(map(self.is_safe, input)))
+
+    def part2(self, input: List[List[int]]) -> None:
+        safe_count = 0
+        for report in input:
+            if any(
+                self.is_safe(report[:i] + report[i + 1 :]) for i in range(len(report))
+            ):
+                safe_count += 1
+        print(safe_count)
+
+
+if __name__ == "__main__":
+    Day02()()
