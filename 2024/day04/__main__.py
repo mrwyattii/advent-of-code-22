12560063360592b30fc104302c1a663e74c2a814
Michael Wyatt <michael.wyatt@snowflake.com>
2024-12-03 22:48:23 -0800

    AOC 24-4 cleanup

--- 2024/day04/__main__.py
+++ 2024/day04/__main__.py
@@ -1,58 +1,54 @@
 from itertools import product
-from typing import List
+from typing import Dict, Tuple
 
 from common.aoc_day import AoCDay
 
 
 class Day04(AoCDay):
-    def process_input(self, raw_input: List[str]):
-        return [[char for char in line.strip()] for line in raw_input]
+    def process_input(self, raw_input: Dict[Tuple[int, int], str]):
+        return {
+            (x, y): char
+            for x, line in enumerate(raw_input)
+            for y, char in enumerate(line.strip())
+        }
 
     @staticmethod
-    def word_in_direction(grid, word, start, direction):
-        x, y = start
-        dx, dy = direction
+    def word_in_direction(
+        grid: Dict[Tuple[int, int], str],
+        word: str,
+        start: Tuple[int, int],
+        direction: Tuple[int, int],
+    ):
         for char in word:
-            if (
-                x < 0
-                or y < 0
-                or x >= len(grid)
-                or y >= len(grid[0])
-                or grid[x][y] != char
-            ):
+            if grid.get(start, "") != char:
                 return False
-            x += dx
-            y += dy
+            start = (start[0] + direction[0], start[1] + direction[1])
         return True
 
-    def part1(self, input: List[List[str]]):
+    def part1(self, input: Dict[Tuple[int, int], str]):
         word_count = 0
         directions = list(product([1, 0, -1], repeat=2))
-        print(directions)
-        for i in range(len(input)):
-            for j in range(len(input[i])):
-                for d in directions:
-                    if self.word_in_direction(input, "XMAS", (i, j), d):
-                        word_count += 1
+        for start in input.keys():
+            for d in directions:
+                if self.word_in_direction(input, "XMAS", start, d):
+                    word_count += 1
         print(word_count)
 
     @staticmethod
-    def x_mas_in_position(grid, x, y):
-        if grid[x][y] != "A":
+    def x_mas_in_position(grid, start):
+        if grid.get(start) != "A":
             return False
-        if x < 1 or y < 1 or x >= len(grid) - 1 or y >= len(grid[0]) - 1:
-            return False
+        x, y = start
         mas_chars = {"M", "S"}
-        forward_slash = set((grid[x - 1][y - 1], grid[x + 1][y + 1]))
-        backward_slash = set((grid[x - 1][y + 1], grid[x + 1][y - 1]))
+        forward_slash = {grid.get((x - 1, y - 1), ""), grid.get((x + 1, y + 1), "")}
+        backward_slash = {grid.get((x - 1, y + 1), ""), grid.get((x + 1, y - 1), "")}
         return (forward_slash == mas_chars) and (backward_slash == mas_chars)
 
-    def part2(self, input: List[List[str]]):
+    def part2(self, input: Dict[Tuple[int, int], str]):
         word_count = 0
-        for i in range(len(input)):
-            for j in range(len(input[i])):
-                if self.x_mas_in_position(input, i, j):
-                    word_count += 1
+        for start in input.keys():
+            if self.x_mas_in_position(input, start):
+                word_count += 1
         print(word_count)
 
 
