9c623fa05e34eddd2aadfde541daef60578aa84a
Michael Wyatt <michael.wyatt@snowflake.com>
2024-12-10 12:52:57 -0800

    AOC 24-10 golfing

--- 2024/day10/__main__.py
+++ 2024/day10/__main__.py
@@ -11,30 +11,21 @@ class Day10(AoCDay):
             for x, cell in enumerate(row.strip())
         }
 
-    def climb(self, topo_map: Dict[complex, int], loc: complex) -> List[complex]:
-        elevation = topo_map[loc]
-
-        if elevation == 9:
+    def climb(self, topo_map: Dict[complex, int], loc: complex, h=0) -> List[complex]:
+        elev = topo_map.get(loc, 0)
+        if elev != h:
+            return []
+        if elev == 9:
             return [loc]
-
-        peaks = []
-        for d in [0 + 1j, 1 + 0j, 0 - 1j, -1 + 0j]:
-            next_loc = loc + d
-            if topo_map.get(next_loc, 0) - elevation == 1:
-                peaks += self.climb(topo_map, next_loc)
-        return peaks
+        return sum(
+            [self.climb(topo_map, loc + d, elev + 1) for d in [1j, 1, -1j, -1]], []
+        )
 
     def part1(self, input: Dict[complex, int]) -> None:
-        trail_score = 0
-        for trail_head in [loc for loc, elev in input.items() if elev == 0]:
-            trail_score += len(set(self.climb(input, trail_head)))
-        print(trail_score)
+        print(sum(len(set(self.climb(input, loc))) for loc in input))
 
     def part2(self, input: Dict[complex, int]) -> None:
-        trail_score = 0
-        for trail_head in [loc for loc, elev in input.items() if elev == 0]:
-            trail_score += len(self.climb(input, trail_head))
-        print(trail_score)
+        print(sum(len(self.climb(input, loc)) for loc in input))
 
 
 if __name__ == "__main__":
