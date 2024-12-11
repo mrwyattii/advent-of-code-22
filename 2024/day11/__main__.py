8d67d276bac6484351bf1d6522aee92c6f85e7c2
Michael Wyatt <michael.wyatt@snowflake.com>
2024-12-10 22:07:33 -0800

    AOC 24-11 visual

--- 2024/day11/__main__.py
+++ 2024/day11/__main__.py
@@ -1,7 +1,9 @@
 from collections import defaultdict
-from typing import DefaultDict, List
+from typing import DefaultDict, List, Tuple
 
+import seaborn as sns
 from common.aoc_day import AoCDay
+from matplotlib import pyplot as plt
 from tqdm import trange
 
 
@@ -27,14 +29,38 @@ class Day11(AoCDay):
                 new_stones[stone_val * 2024] += num_stones
         return new_stones
 
+    def visualize(self, stone_history: List[Tuple[int, int]]) -> None:
+        _, ax = plt.subplots(figsize=(10, 6))
+        unique_stones, stone_count = zip(*stone_history)
+        sns.lineplot(
+            x=range(len(stone_history)),
+            y=unique_stones,
+            ax=ax,
+            label="Unique Stones",
+            color="g",
+        )
+        sns.lineplot(
+            x=range(len(stone_history)),
+            y=stone_count,
+            ax=ax,
+            label="Total Stones",
+            color="b",
+        )
+        ax.set_yscale("log")
+        ax.set_ylim(1, None)
+        plt.savefig("day11/day11.png")
+
     def part1(self, input: DefaultDict[int, int]) -> None:
         for _ in trange(25):
             input = self.blink(input)
         print(sum(input.values()))
 
     def part2(self, input: DefaultDict[int, int]) -> None:
+        stone_history = [(len(input), sum(input.values()))]
         for _ in trange(75):
             input = self.blink(input)
+            stone_history.append((len(input), sum(input.values())))
+        self.visualize(stone_history)
         print(sum(input.values()))
 
 
